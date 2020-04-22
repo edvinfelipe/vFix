693ad4d4b018596548675d3153d68599496f5f47
CarlosMenchu2 <36994539+CarlosMenchu2@users.noreply.github.com>
2020-04-21 21:49:26 -0600

    Se agrego el formulario del Login con toda la funcionalidad que necesita

--- VFIX/src/main/java/com/mycompany/vfix/General.java
+++ VFIX/src/main/java/com/mycompany/vfix/General.java
@@ -26,6 +26,7 @@ public class General extends javax.swing.JFrame {
      */
     public General() {
         initComponents();
+        this.setLocationRelativeTo(null);
         
       
     }
@@ -148,8 +149,10 @@ public class General extends javax.swing.JFrame {
         getContentPane().add(jPanel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 190, 700));
 
         jPanel2.setBackground(new java.awt.Color(54, 159, 128));
+        jPanel2.setFont(new java.awt.Font("Tahoma", 0, 24)); // NOI18N
 
-        jLabel1.setFont(new java.awt.Font("Tahoma", 0, 24)); // NOI18N
+        jLabel1.setFont(new java.awt.Font("Tahoma", 0, 36)); // NOI18N
+        jLabel1.setForeground(new java.awt.Color(255, 255, 255));
         jLabel1.setText("Vfix");
 
         jButton1.setBackground(new java.awt.Color(54, 159, 128));
@@ -175,9 +178,9 @@ public class General extends javax.swing.JFrame {
         jPanel2Layout.setHorizontalGroup(
             jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel2Layout.createSequentialGroup()
-                .addContainerGap(517, Short.MAX_VALUE)
+                .addContainerGap(506, Short.MAX_VALUE)
                 .addComponent(jLabel1)
-                .addGap(368, 368, 368)
+                .addGap(360, 360, 360)
                 .addComponent(btnMinimizar)
                 .addGap(36, 36, 36)
                 .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 37, javax.swing.GroupLayout.PREFERRED_SIZE)
@@ -187,11 +190,14 @@ public class General extends javax.swing.JFrame {
             jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel2Layout.createSequentialGroup()
                 .addContainerGap()
-                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jButton1)
-                    .addComponent(btnMinimizar)
-                    .addComponent(jLabel1))
-                .addContainerGap(18, Short.MAX_VALUE))
+                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(jPanel2Layout.createSequentialGroup()
+                        .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(jButton1)
+                            .addComponent(btnMinimizar))
+                        .addGap(0, 0, Short.MAX_VALUE))
+                    .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addContainerGap())
         );
 
         getContentPane().add(jPanel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(190, 0, 1080, 60));
---
+++ VFIX/src/main/java/com/mycompany/vfix/InicioSesion/Login.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.vfix.InicioSesion;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author carlo
+ */
+public class Login implements Serializable{
+    
+    private String token;
+
+    public String getToken() {
+        return token;
+    }
+    
+    
+    
+    
+}
